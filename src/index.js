f0eb40cd320fc4799b925788ab8996b977b35535
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2016-05-16 20:56:12 -0400

    Adds clear functionality for posts and puts

--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ function cache() {
     check: url => _cache.hasOwnProperty(url),
     set: (url, payload) => { _cache[url] = payload; },
     get: url => _cache[url],
+    clear: url =>
+      Object.keys(_cache).forEach(key => {
+        let listUrl = key.substr(0, key.lastIndexOf('?'));
+        let getByIdUrl = key.substr(0, key.lastIndexOf('/'));
+        if (key.includes('?') && key.includes(listUrl)) {
+          delete _cache[key];
+        } else if (key.includes(getByIdUrl)) {
+          delete _cache[key];
+        }
+      })
   };
 }
 
@@ -18,22 +28,43 @@ function isGetRequest({ payload: { params } }) {
   return params.method === 'GET';
 }
 
+function isPostRequest({ payload: { params } }) {
+  return params.method === 'POST';
+}
+
+function isPutRequest({ payload: { params } }) {
+  return params.method === 'PUT';
+}
+
 export default function httpCache() {
-  const { get, set, check } = cache();
+  const { get, set, check, clear } = cache();
 
   return next => action => {
+
+    let url;
+
+    if (isRequestType(action)) {
+      url = action.payload.url;
+      if (isPostRequest(action)) {
+        clear(url);
+      }
+      if (isPutRequest(action)) {
+        clear(url);
+      }
+    }
+
     if (!isRequestType(action) || !isGetRequest(action)) {
       return next(action);
     }
 
-    if (check(action.payload.url)) {
-      return get(action.payload.url);
+    if (check(url)) {
+      return get(url);
     }
 
     const nextAction = next(action);
 
     // Cache the action!
-    set(action.payload.url, nextAction);
+    set(url, nextAction);
 
     return nextAction;
   };
